95d1b43127fd02fc0095ab6401fdedf4e5137d84
t.me/xtekky <98614666+xtekky@users.noreply.github.com>
2023-01-02 13:15:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -37,23 +37,11 @@ __keys__ = {
 }
 
 class livecounts:
-    @staticmethod
-    def __signature(timestamp: int) -> dict:
-        
-        return {
-            'x-aurora' : str(3 * timestamp),
-            'x-joey'   : str(timestamp),
-            'x-maven'  : sha256(f"0AVwElhWi1IfwcZKSNzq7E^84hFQ4ykenNAxeY7r@6ho1oTd6Ug*!WC&p$2aGY8MLHEkH0i8XCwnj3#JqI1NzCb91$gNzLYCbbG@NqvQMbcf8W9v3%s#uzjP@z*!e9a41JNWBqRIMJ*ULuav5k8z4kBj2^BCC%!3q@N0zZOS^TL#GzVz@9fhjg&^mSWi&oU5GMoCu9{timestamp}".encode()).hexdigest(),
-            'x-mayhem' : "553246736447566b58312f7a4f72413653425342717a6e4231596f7a4d59686564764842324b396478544443756669734d56706f4346334633456366724b6732",
-            'x-midas'  : sha256(str(timestamp + 64).encode()).hexdigest()
-        }
     
     @staticmethod
     def video_info(video_id: (int or str)) -> dict:
-        timestamp = int(time() * 1000)
         
         headers = {
-            **livecounts.__signature(timestamp),
             'authority' : 'tiktok.livecounts.io',
             'origin'    : 'https://livecounts.io',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
